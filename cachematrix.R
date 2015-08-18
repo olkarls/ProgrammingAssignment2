fe00d776d6aeae8dbaa86c26d5b81c115895e599
Ola Karlsson <olkarls@gmail.com>
2015-08-18 23:07:04 +0200

    Solves the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix creates an object vid functions to get and set the matrix
+## and to get and set the inversion.
 makeCacheMatrix <- function(x = matrix()) {
+  matrix <- x
+  inversion <- NULL
+  set <- function(x) {
+    matrix <<- x
+    inversion <<- NULL
+  }
 
+  get <- function() { matrix }
+  setinversion <- function(solved) { inversion <<- solved }
+  getinversion <- function() { inversion }
+  list(set = set, get = get, setinversion = setinversion, getinversion = getinversion)
 }
 
 
-## Write a short comment describing this function
+## Takes an matrix as argument and returns the inversion of it
+## if the inversion is alread calculated it returns the cached value
+## otherwise it sets it and clears the cache.
+cacheSolve <- function(x) {
+  inversion <- x$getinversion()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!is.null(inversion)) {
+    inversion
+  }
+
+  matrix <- x$get()
+  inversion <- solve(matrix)
+  x$setinversion(inversion)
 }
